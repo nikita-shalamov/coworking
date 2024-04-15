f42a2d1e2946e38bbe79c8a7ac6c629e5b3408a4
crasavella04 <andreyberd04@gmail.com>
2024-04-15 21:53:29 +0300

    remove app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { ConfigProvider } from "antd";
 import { antdConfig } from "./config/antd.ts";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+import "./styles/style.sass"
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+    interface Register {
+      router: typeof router
+    }
+  }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
   <React.StrictMode>
     <ConfigProvider theme={{ token: antdConfig }}>
-      <App />
+      <RouterProvider router={router} />
     </ConfigProvider>
   </React.StrictMode>
 );
