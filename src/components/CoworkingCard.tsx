978ce1bb959fa2e7352fda443b3e978ced1c154e
Nikita Shalamov <nikita.shalamovv@yandex.ru>
2024-04-16 16:37:55 +0500

    .

--- src/components/CoworkingCard.tsx
+++ src/components/CoworkingCard.tsx
@@ -1,4 +1,9 @@
-import { Badge, Button } from "antd"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRef, useState } from "react"
+
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Badge, Button, Carousel } from "antd"
 
 interface coworkingCardProps {
     title: string;
@@ -6,9 +11,11 @@ interface coworkingCardProps {
     price: number;
     link: string;
     rating: number;
+    metro: string;
+    address: string;
 }
 
-export default function CoworkingCard({ title, image, price, link, rating }: coworkingCardProps) {
+export default function CoworkingCard({ title, image, price, link, rating, metro, address }: coworkingCardProps) {
     const backgroundColorRating = (rating: number) => {
         if (rating <= 4 && rating >= 3) {
             return "#faad14"
@@ -19,14 +26,60 @@ export default function CoworkingCard({ title, image, price, link, rating }: cow
             return "#f5222d"
         }
     }
-    console.log(rating)
+
+    const contentStyle: React.CSSProperties = {
+        margin: 0,
+        height: "100%",
+        width: "100%",
+        color: "#fff",
+        textAlign: "center",
+        background: "#364d79",
+    }
+
+    const ref = useRef<any>(null)
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const nextSlide = () => {
+        setCurrentIndex(prev => prev + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrentIndex(prev => prev + 1)
+    }
+
     return (
         <div className="coworking_card">
             <div className="coworking_card__wrapper">
                 <Badge className="coworking_raiting" count={"Рейтинг: " + rating + " / 5"} style={{ backgroundColor: backgroundColorRating(rating) }} />
                 <div className="coworking_card__col coworking_card__col-1">
-                    <div className="coworking_card-image">
-                        <img src={image} alt={title} />
+                    <div className="coworking_card__carousel">
+                        <Carousel ref={ref}>
+                            <div className="coworking_card-image" style={contentStyle}>
+                                <img src={image} alt={title} />
+                            </div>
+                            <div className="coworking_card-image" style={contentStyle}>
+                                <img src={image} alt={title} />
+                            </div>
+                            <div className="coworking_card-image" style={contentStyle}>
+                                <img src={image} alt={title} />
+                            </div>
+                        </Carousel>
+                        <div
+                            className="coworking_card__arrow_prev"
+                            onClick={() => {
+                                ref.current?.prev?.()
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </div>
+                        <div
+                            className="coworking_card__arrow_next"
+                            onClick={() => {
+                                ref.current?.next?.()
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </div>
                     </div>
                 </div>
                 <div className="coworking_card__col">
@@ -34,6 +87,17 @@ export default function CoworkingCard({ title, image, price, link, rating }: cow
                         <div className="coworking_card-title">{title}</div>
                         <div className="coworking_card-price">Стоимость за час: от {price.toFixed(2)}р.</div>
                         <div className="coworking_card-options">Безлимитный кофе · Удобные места · Больше окна</div>
+                        <div className="coworking_card-location">
+                            <img
+                                width="15"
+                                height="15"
+                                src="https://img.icons8.com/external-others-inmotus-design/67/external-Subway-metro-others-inmotus-design-14.png"
+                                alt="external-Subway-metro-others-inmotus-design-14"
+                            />
+                            <div className="coworking_card-metro">{metro}</div>
+                            <img width="15" height="15" src="https://img.icons8.com/material-rounded/24/visit.png" alt="visit" />
+                            <div className="coworking_card-address">{address}</div>
+                        </div>
                     </div>
                     <div className="coworking_card-button__wrapper">
                         <Button className="coworking_card-button" type="primary" href={link}>
--- src/components/CoworkingList.tsx
+++ src/components/CoworkingList.tsx
@@ -4,26 +4,35 @@ import SearchForm from "./SearchForm"
 
 export default function CoworkingList() {
     const coworkings = [
-        {
+        {   
+            id: 1,
             title: "Коворкинг 1",
             image: "../images/coworkings/image.png",
             price: 199,
             link: "https://t.me/nikita_frl",
             rating: 4,
+            metro: 'м. Крестовский остров',
+            address: 'ул. Морской просп., 25'
         },
         {
+            id: 2,
             title: "Коворкинг 1",
             image: "../images/coworkings/image.png",
             price: 200,
             link: "https://t.me/nikita_frl",
             rating: 5,
+            metro: 'м. Крестовский остров',
+            address: 'ул. Морской просп., 25'
         },
         {
+            id: 3,
             title: "Коворкинг 1",
             image: "../images/coworkings/image.png",
             price: 201,
             link: "https://t.me/nikita_frl",
             rating: 2,
+            metro: 'м. Крестовский остров',
+            address: 'ул. Морской просп., 25'
         },
     ]
     return (
@@ -36,7 +45,7 @@ export default function CoworkingList() {
                             <SearchForm />
                             <h1 className={"coworking_list__title title"}>Список коворкингов</h1>
                             {coworkings.map(coworking => {
-                                return <CoworkingCard title={coworking.title} image={coworking.image} price={coworking.price} link={coworking.link} rating={coworking.rating} />
+                                return <CoworkingCard key={coworking.id} {...coworking} />
                             })}
                         </div>
                     </div>
